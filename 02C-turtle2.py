391f377c4b34233e690fbec460cc6ca1b2dd7981
bsh0137 <bookert0137@naver.com>
2020-02-04 15:18:17 +0900

    이름 변경

---
+++ 02C-turtle2.py
@@ -0,0 +1,20 @@
+import turtle as t
+t.shape("turtle")
+
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+t.forward(100)
+t.lt(90)
+t.forward(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+t.circle(50)
